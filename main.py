e3269828f6594da4118fe36df30aef4ea1b36f9c
AhmedAmgadElsharkawy <ahmedamgad16746@gmail.com>
2026-04-24 22:55:42 +0300

    add project structure

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from view.window.main_window import MainWindow
+from PyQt5.QtGui import QIcon
+
+def main():
+    app = QApplication(sys.argv)
+    # app.setWindowIcon(QIcon("assets/icons/favicon.png"))
+    main_window = MainWindow()
+    main_window.showMaximized()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
